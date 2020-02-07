0d78114cc3a1944ce6c91c635727ba016be32382
Karan Kumar <nailedITnerd@gmail.com>
2020-02-07 23:19:51 +0530

    Added base logic

--- lookup/views.py
+++ lookup/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
+#API LINK 
+'''
+http://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=20002&distance=25&API_KEY=CB02EA06-D4B1-459D-9A3F-3005A8692D8D
+'''
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    apiRequest = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=CB02EA06-D4B1-459D-9A3F-3005A8692D8D")
+
+    try:
+        api = json.loads(apiRequest.content)
+    except Exception as e:
+        api = "Error"
+
+    return render(request, 'home.html', {'api': api})
