6bab6bb528387218e86a14c7b65f8afcb2225d15
Karan Kumar <nailedITnerd@gmail.com>
2020-02-08 14:15:07 +0530

    Added color codes

--- lookup/views.py
+++ lookup/views.py
@@ -14,5 +14,19 @@ def home(request):
         api = json.loads(apiRequest.content)
     except Exception as e:
         api = "Error"
+    
+    if api[0]['Category']['Name'] == "Good":
+            category_description = "Air quality is considered satisfactory, and air pollution poses little or no risk."
+    elif api[0]['Category']['Name'] == "Moderate":
+            category_description = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+    elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+            category_description = "Although general public is not likely to be affected at this AQI range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas persons with heart and lung disease, older adults and children are at greater risk from the presence of particles in the air."
+    elif api[0]['Category']['Name'] == "Unhealthy":
+            category_description = "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
+    elif api[0]['Category']['Name'] == "Very Unhealthy":
+            category_description = "Health alert: everyone may experience more serious health effects."
+    elif api[0]['Category']['Name'] == "Hazardous":
+            category_description = "Health warnings of emergency conditions. The entire population is more likely to be affected."
+        
 
-    return render(request, 'home.html', {'api': api})
+    return render(request, 'home.html', {'api': api, 'category_description': category_description})
